f1aca1796dfb597d94cd99538e7f1582bec96fd7
Eric Smith <eric@8thlight.com>
2010-12-10 07:03:17 -0600

    Added conjure to the dependencies, and chmodded conjure.sh to make it executable

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject my-homework-assignment "1.0.0-SNAPSHOT"
   :description "A Demonstration of using a leinengen plugin"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [conjure-core "0.8.0"]]
+  :dev-dependencies [[lein-conjure "0.8.0"]])
