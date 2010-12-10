f2e120cf93c838e456f0a6d4d1e629231b6c816a
Eric Smith <eric@8thlight.com>
2010-12-10 06:59:17 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject my-homework-assignment "1.0.0-SNAPSHOT"
+  :description "A Demonstration of using a leinengen plugin"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ src/my_homework_assignment/core.clj
@@ -0,0 +1,1 @@
+(ns my-homework-assignment.core)
